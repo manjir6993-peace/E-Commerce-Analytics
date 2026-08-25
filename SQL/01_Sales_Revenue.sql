d128eb1d08a52c6327e90aa2bd95d785b192541e
manjir6993-peace <manjir6993@gmail.com>
2026-08-25 19:10:07 +0530

    Add Sales & Revenue analysis structure

--- SQL/01_Sales_Revenue.sql
+++ SQL/01_Sales_Revenue.sql
@@ -1,1 +1,23 @@
+========================================================
+E-COMMERCE ANALYTICS PROJECT
+01 - SALES & REVENUE ANALYSIS
+========================================================
+
+Dataset:
+Olist Brazilian E-Commerce Dataset
+
+Database:
+ecommerce_analytics
+
+Business Questions:
+1. How are orders distributed by status?
+2. How many orders are placed each year?
+3. How much revenue is generated each year?
+4. What is the Average Order Value (AOV) by year?
+5. What are the monthly sales trends?
+
+Main Tables:
+- orders
+- order_items
+========================================================
 
